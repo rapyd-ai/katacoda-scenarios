f87efef70131f1f8008250539a34570b5d3c0d44
tobiaszwingmann <tobi@Tobiass-MBP-15.fritz.box>
2020-04-13 22:44:06 +0200

    Updated steps for user review classification use case

--- text-cleaning-nlp/assets/data_reader.py
+++
@@ -1,9 +0,0 @@
-from nltk.corpus import movie_reviews
-
-def read_reviews():
-    documents = []
-
-    for file_id in movie_reviews.fileids():
-        documents.append(movie_reviews.raw(file_id))
-
-    return documents
--- text-cleaning-nlp/finish.md
+++
@@ -1,1 +0,0 @@
-You've completed Text Cleaning scenario.
